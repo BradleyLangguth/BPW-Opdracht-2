7300f52a4245745ffd4726a4f7452ea4d6b72d8e
BradleyLangguth <35529740+BradleyLangguth@users.noreply.github.com>
2018-06-29 21:32:02 +0200

    Initial commit

---
+++ Building Playfull Worlds, Opdacht 2/Assets/Code/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    
+
+
+ public void ChangeScene(string scenename)
+ {
+  Application.LoadLevel(scenename);
+ }
+    
+ public void Exit()
+ {
+  Application.Quit();
+ }
+
+}
